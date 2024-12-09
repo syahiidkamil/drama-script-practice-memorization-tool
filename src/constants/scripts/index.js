429339884af9d7aff7653fb4ae62a47c30e89568
Syahiid Nur Kamil <syahiidkamil.tech@gmail.com>
2024-12-09 15:26:19 +0700

    update index export

--- src/constants/scripts/index.js
+++ src/constants/scripts/index.js
@@ -2,3 +2,8 @@ export * from "./scene1Script";
 export * from "./scene2Script";
 export * from "./scene3Script";
 export * from "./scene4Script";
+export * from "./scene5Script";
+export * from "./scene6Script";
+export * from "./scene7Script";
+export * from "./scene8Script";
+export * from "./scene9Script";
